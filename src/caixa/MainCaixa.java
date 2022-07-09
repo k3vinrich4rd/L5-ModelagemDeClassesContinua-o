5177d4270aacc9d5349c780f610188bbdfa4768d
k3vinrich4rd <kevinrichardve@gmail.com>
2022-07-08 22:17:01 -0300

    Inserindo atualizações

--- src/caixa/MainCaixa.java
+++ src/caixa/MainCaixa.java
@@ -12,25 +12,41 @@ public class MainCaixa {
 
         double valorRecebido;
         double soma = 0;
-
         List<Double> credito = new ArrayList<>();
-
         String respostas = "";
-        System.out.print("Digite o números de lançamentos que você deseja fazer: ");
-        int lancamentos = input.nextInt();
+        do {
 
-        System.out.print("Você quer fazer no crédito ou no débito?:  ");
-        respostas = input.next();
-        if (respostas.equalsIgnoreCase("credito")) {
-            for (double contador = 0; contador < lancamentos; contador++) {
-                System.out.print("Digite o " + (contador + 1) + "º valor: ");
-                credito.add( input.nextDouble());
-                soma += (double) credito.get((int) contador);
-                caixa.credito = soma;
+            System.out.print("Digite o números de lançamentos que você deseja fazer: ");
+            int lancamentos = input.nextInt();
+
+            System.out.print("Você quer fazer no crédito ou no débito?:  ");
+            respostas = input.next();
+            if (respostas.equalsIgnoreCase("credito")) {
+                for (double contador = 0; contador < lancamentos; contador++) {
+                    System.out.print("Digite o " + (contador + 1) + "º valor: ");
+                    credito.add(input.nextDouble());
+                    soma += (double) credito.get((int) contador);
+                    caixa.credito = soma;
+                }
+                System.out.println(caixa.credito());
+
             }
-            System.out.println(caixa.credito());
+            if (respostas.equalsIgnoreCase("debito")) {
+                for (double contador = 0; contador < lancamentos; contador++) {
+                    System.out.print("Digite o " + (contador + 1) + "º valor: ");
+                    credito.add(input.nextDouble());
+                    soma += (double) credito.get((int) contador);
+                    caixa.debito = soma;
+                }
+                System.out.println(caixa.debito());
 
-        }
+
+
+            }
+            System.out.println("Você deseja fazer mais alguma movimentação? [S/N]: ");
+            respostas = input.next();
+        }while (respostas.equalsIgnoreCase("s"));
+        System.out.println("Agradecemos pele preferência, até a próxima.");
 
 
     }
